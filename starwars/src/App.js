1be9bfc4c9ec9cd10c87802c59f7459a3f9461dd
derek-cornelison <dcornelison1216@gmail.com>
2020-04-17 21:52:38 -0400

    more proofreading

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import CharGrid from ''./components/CharGrid';
+import CharGrid from './components/CharGrid';
 import styled from 'styled-components';
 
 const Header = styled.h1`
