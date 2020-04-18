a4bfb37b2b450b1f301ce05ae2585daf29b0f8cb
derek-cornelison <dcornelison1216@gmail.com>
2020-04-17 21:11:13 -0400

    initial commit - installed dependencies, page loading

---
+++ starwars/src/components/CharCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Card, Col } from "reactstrap";
+
+const StyledImg = styled.img`
+max-width: 50%;
+max-height: 250px;
+`;
+
+const Name = styled.h2`
+margin: .8rem auto;
+color: #443e3e;
+text-shadow: 1px 1px 5px #fff;
+`;
+
+const Gender = styled.p`
+margin: .5rem 0rem;
+text-align: center;
+text-transform: capitalize;
+text-shadow: 1px 1px 5px #fff;
+`;
+
+const Details = styled.p`
+margin: .5rem 0rem;
+text-align: center;
+text-transform: capitalize;
+text-shadow: 1px 1px 5px #fff;
+`;
+
+const CharCard = props => {
+  return (
+    <Col>
+      <Card className="char-card">
+        <Name>{props.name}</Name>
+        <Gender>{props.gender}</Gender>
+        <Details>Height: {props.height} m</Details>
+        <Details>Mass: {props.mass} kg</Details>
+      </Card>
+    </Col>
+  );
+};
+
+export default CharCard;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,41 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import CharCard from './CharCard';
+
+const Container = styled.div`
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: space-around;
+`;
+
+export default function CharGrid() {
+  const [photoData, setPhotoData] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get("https://swapi.py4e.com/api/people/")
+    .then(response => {
+      setPhotoData(response.data.results);
+      console.log(response.data.results);
+    })
+    .catch(error => {
+      console.log("The data was not returned", error);
+    });
+  }, []);
+
+  return (
+    <Container>
+      {photoData.map((char, index) => {
+        return <CharCard
+        name={char.name}
+        key={index}
+        height={char.height}
+        mass={char.mass}
+        gender={char.gender} />
+      })}
+    </Container>
+  );
+}
